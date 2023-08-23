40f6d14ae675e1a7469caf169efa9914dda30acd
DJJuampi <juanpabloalvira@gmail.com>
2023-08-23 16:48:35 -0300

    First Commit

---
+++ My project/Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Variables a configurar desde el editor
+    [Header("Configuracion")]
+    [SerializeField] float velocidad = 5f;
+    [SerializeField] private float fuerzaSalto = 5f;
+
+    // Variables de uso interno en el script
+    private float moverHorizontal;
+    private Vector2 direccion;
+     private bool puedoSaltar = true;
+    private bool saltando = false;
+
+    // Variable para referenciar otro componente del objeto
+    private Rigidbody2D miRigidbody2D;
+
+    // Codigo ejecutado cuando el objeto se activa en el nivel
+    private void OnEnable()
+    {
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Codigo ejecutado en cada frame del juego (Intervalo variable)
+    private void Update()
+    {
+        moverHorizontal = Input.GetAxis("Horizontal");
+        direccion = new Vector2(moverHorizontal, 0f);
+
+         if (Input.GetKeyDown(KeyCode.Space) && puedoSaltar)
+        {
+            puedoSaltar = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        miRigidbody2D.AddForce(direccion * velocidad);
+
+         if (!puedoSaltar && !saltando)
+        {
+            miRigidbody2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            saltando = true;
+        }
+    }
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        puedoSaltar = true;
+        saltando = false;
+    }
+}
